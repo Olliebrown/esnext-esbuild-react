45ec4ad18ec46ef0e72290322c888c231310294d
Seth Berrier <berriers@uwstout.edu>
2023-03-13 15:32:25 -0500

    Tweeks to public dir tracking

--- build.js
+++ build.js
@@ -11,57 +11,22 @@ const _DEV_ = process.argv.some((arg) => (arg.toLowerCase() === 'dev'))
 const options = {
   // List of all separate entry points
   entryPoints: [
-<<<<<<< HEAD
-    './client/app.jsx'
-=======
     './client/main.jsx'
->>>>>>> 122c08c (Big update to react 18 and esbuild 0.17)
   ],
 
   // Configure output location and names
   outdir: './public',
   entryNames: '/[name]',
 
-<<<<<<< HEAD
-  // Configure output types
-  bundle: true,
-  sourcemap: _DEV_,
-  minify: (!_DEV_),
-=======
   // Configure output types and target
   bundle: true,
   sourcemap: _DEV_,
   minify: (!_DEV_),
   target: 'es6',
->>>>>>> 122c08c (Big update to react 18 and esbuild 0.17)
 
   // Define important variables
   define: {
     _VER_: `"${process.env.npm_package_version}"`,
-<<<<<<< HEAD
-    _DEV_,
-    'process.env.NODE_ENV': (_DEV_ ? '"development"' : '"production"')
-  }
-}
-
-// Start up server if requested
-if (serve) {
-  ESBuild.serve({ port: 3000, servedir: 'public' }, options)
-    .then(server => {
-      console.log(`Serving dev code at http://localhost:${server.port}`)
-      const handleExit = (signal) => {
-        console.log('Exiting due to ' + signal)
-        server.stop()
-      }
-      process.on('SIGINT', handleExit)
-      process.on('SIGTERM', handleExit)
-    })
-    .catch((err) => {
-      console.error('Failed to start server')
-      console.error(err)
-      process.exit(1)
-    })
-=======
     _DEV_: JSON.stringify(_DEV_)
   }
 }
@@ -95,7 +60,6 @@ if (serve || _DEV_) {
     console.error(err)
     process.exit(1)
   }
->>>>>>> 122c08c (Big update to react 18 and esbuild 0.17)
 } else {
   // Attempt to build
   ESBuild.build(options).catch(
